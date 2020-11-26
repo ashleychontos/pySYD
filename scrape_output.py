d41f35ce1e7e51c0515c5d21f22e7c27c3c64b97
Ashley Chontos <ashleychontos@gmail.com>
2020-11-25 14:08:22 -1000

    Added MC plots and fixed scrape_output paths

--- scrape_output.py
+++ scrape_output.py
@@ -5,22 +5,22 @@ def _csv_reader(f):
     row = pd.read_csv(f,header=None,squeeze=True, index_col=0)
     return row
 
-path='Files/todo/*findex*'
+path = 'Files/results/**/*findex.csv'
 fL = glob.glob(path)
 df = pd.read_csv(fL[0])
 
 for i in range(1,len(fL)):
-	dfnew=pd.read_csv(fL[i])
-	df = pd.concat([df,dfnew])
+	   dfnew = pd.read_csv(fL[i])
+	   df = pd.concat([df,dfnew])
 
-df.to_csv('findex.csv',index=False)
+df.to_csv('Files/findex.csv',index=False)
 
-path='Files/todo/*globalpars*'
+path = 'Files/results/**/*globalpars.csv'
 fL = glob.glob(path)
 df = pd.read_csv(fL[0])
 
 for i in range(1,len(fL)):
-	dfnew=pd.read_csv(fL[i])
-	df = pd.concat([df,dfnew])
+	   dfnew = pd.read_csv(fL[i])
+	   df = pd.concat([df,dfnew])
 
-df.to_csv('globalpars.csv',index=False)
+df.to_csv('Files/globalpars.csv',index=False)
