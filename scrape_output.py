14a9fd025d9911a4279d6df2116d08bf5f4029cb
Maryum Sayeed <maryum.sayeed@alumni.ubc.ca>
2021-02-27 16:19:11 -0700

    fixed finding peaks function; modified scrape_output to include errors in globalpars.csv

--- scrape_output.py
+++ scrape_output.py
@@ -5,6 +5,7 @@ Grabs each individual target's results and concatenates results into a single cs
 
 import glob
 import pandas as pd
+from itertools import chain
 
 
 # TODO: This doesn't seem to be used
@@ -31,11 +32,16 @@ files = glob.glob(path)
 df = pd.DataFrame(columns=['target'])
 
 for i, file in enumerate(files):
-    df_new = pd.read_csv(file)
-    df_new.set_index('parameter', inplace=True, drop=False)
-    df.loc[i, 'target'] = file.strip().split('/')[-2]
-    for col in df_new.index.values.tolist():
-        df.loc[i, col] = df_new.loc[col, 'value']
+	df_new = pd.read_csv(file)
+	df_new.set_index('parameter',inplace=True,drop=False)
+	df.loc[i,'target']=file.strip().split('/')[-2]
+	new_header_names=[[i,i+'_err'] for i in df_new.index.values.tolist()] #add columns to get error
+	new_header_names=list(chain.from_iterable(new_header_names))          
+	for col in new_header_names:
+		if '_err' in col:
+			df.loc[i,col]=df_new.loc[col[:-4],'uncertainty']
+		else:
+			df.loc[i,col]=df_new.loc[col,'value']
 
 df.fillna('--', inplace=True)
 df.to_csv('Files/globalpars.csv', index=False)
