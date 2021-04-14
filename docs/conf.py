30952a3f81b4bf5e3c64526d18e22a7dc8b63aef
Ashley Chontos <ashleychontos@gmail.com>
2021-04-13 15:39:45 -1000

    more docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+#
+# pySYD documentation buld configuration file, created by
+# sphinx-quickstart on 
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -13,16 +18,19 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import pysyd
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'pySYD'
-copyright = '2021, Ashley Chontos, Daniel Huber, Maryum Sayeed, and contributors'
-author = 'Ashley Chontos, Daniel Huber, Maryum Sayeed, and contributors'
+project = u'pySYD'
+copyright = u'2021, Ashley Chontos, Daniel Huber, and Maryum Sayeed'
+author = u'Ashley Chontos, Daniel Huber, and Maryum Sayeed'
 
+# The short X.Y version.
+version = '.'.join(pysyd.__version__.split('.')[1:])
 # The full version, including alpha/beta/rc tags
-release = '0.4.2'
+release = pysyd.__version__
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,8 +38,17 @@ release = '0.4.2'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
 extensions = [
 ]
+
+# The suffix(es) of source filesnames.
+# You can specify multiple suffix as a list of string:
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,7 +64,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'asteroid-sphinx-theme'
+html_theme = 'asteroid_sphinx_theme'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
@@ -56,9 +73,12 @@ html_logo = "figures/pysyd_logo_black_text.png"
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = "plots/radvel_logo_favicon.ico"
+#html_favicon = "figures/pysyd_logo_favicon.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'pysyddoc'
