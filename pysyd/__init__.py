577ab0bf7501ca6f568c350633f304ed38d2ced2
Ashley Chontos <ashleychontos@gmail.com>
2021-04-17 14:34:45 -1000

    ddfa

--- pysyd/__init__.py
+++ pysyd/__init__.py
@@ -9,7 +9,7 @@ from .utils import *
 
 __all__ = ['cli', 'functions', 'pipeline', 'models', 'target', 'plots', 'utils']
 
-__version__ = '0.4.5'
+__version__ = '0.4.6'
 
 _ROOT = os.path.abspath(os.getcwd())
 TODODIR = os.path.join(_ROOT, 'info', 'todo.txt')
