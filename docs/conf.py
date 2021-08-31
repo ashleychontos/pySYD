b2ff523bf1d718d5d8905b6e0abff1dc3cda5399
Ashley Chontos <ashleychontos@gmail.com>
2021-08-31 10:20:18 -1000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -68,7 +68,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
-    'sphinx.ext.pngmath',
+#    'sphinx.ext.pngmath',
     'sphinx.ext.ifconfig',
     'matplotlib.sphinxext.only_directives',
     'matplotlib.sphinxext.plot_directive',
