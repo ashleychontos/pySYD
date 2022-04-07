ab90e8b6f7db151d8db960b44feba05e73f2b15d
Ashley Chontos <ashleychontos@gmail.com>
2022-04-07 13:41:12 -0400

    jkdfadkjj

--- pysyd/__init__.py
+++ pysyd/__init__.py
@@ -8,7 +8,7 @@ import sys
 
 __all__ = ['cli', 'pipeline', 'models', 'target', 'plots', 'utils']
 __author__ = 'ashley <achontos@hawaii.edu>'
-__version__ = '5.0.7'
+from version import __version__ 
 
 _ROOT = os.path.abspath(os.getcwd())
 INFDIR = os.path.join(_ROOT, 'info')
