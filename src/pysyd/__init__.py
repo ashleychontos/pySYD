517f3902c613c5136259ee6a34197a46afea63f3
Ashley Chontos <ashleychontos@gmail.com>
2022-11-02 13:53:12 -0400

    update init file

--- src/pysyd/__init__.py
+++ src/pysyd/__init__.py
@@ -1,8 +1,29 @@
+__bibtex__ = """
+@article{pysyd,
+       author = {{Chontos}, Ashley and {Huber}, Daniel and {Sayeed}, Maryum and {Yamsiri}, Pavadol},
+        title = "{$\texttt{pySYD}$: Automated measurements of global asteroseismic parameters}",
+      journal = {arXiv e-prints},
+     keywords = {Astrophysics - Solar and Stellar Astrophysics, Astrophysics - Instrumentation and Methods for Astrophysics},
+         year = 2021,
+        month = aug,
+          eid = {arXiv:2108.00582},
+        pages = {arXiv:2108.00582},
+archivePrefix = {arXiv},
+       eprint = {2108.00582},
+ primaryClass = {astro-ph.SR}, 
+       adsurl = {https://ui.adsabs.harvard.edu/abs/2021arXiv210800582C},
+      adsnote = {Provided by the SAO/NASA Astrophysics Data System}
+}
+"""
+__url__ = "https://pysyd.readthedocs.io"
+__author__ = "Ashley Chontos<ashleychontos@astro.princeton.edu>"
+__license__ = "MIT"
+__description__ = "Automated measurements of global asteroseismic parameters"
+
 import os
 import sys 
 
 __all__ = ['cli','pipeline','models','target','plots','utils']
-__author__ = 'ashley <ashleychontos@astro.princeton.edu>'
 from .version import __version__ 
 
 # Directory with personal pysyd data & info
@@ -27,7 +48,6 @@ __minimum_python_version__ = "3.8"
 
 class PythonNotSupportedError(Exception):
     pass
-
 
 if sys.version_info < tuple((int(val) for val in __minimum_python_version__.split('.'))):
     raise PythonNotSupportedError(
